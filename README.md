885c0ee849bf33c1554163d4c963eca23578a087
Prudhvi <pruthvipatnala@usc-guestwireless-upc-newsc8772.usc.edu>
2021-11-15 18:43:47 -0800

    bootstrap done

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # mola_twitter_ui
 Sample UI to check if a user is present in Twitter and download their latest tweets
+
+### Steps:
+
+- python3 server.py
+- cd ui
+- npm start
--- ui/src/App.js
+++ ui/src/App.js
@@ -1,8 +1,10 @@
 import { useState, useEffect, useContext } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Button, InputGroup, FormControl, Container, Row, Col } from 'react-bootstrap';
 
 
-
-const App = ({}) => {
+const App = ({ }) => {
   // const state = useContext(StateContext);
   const [username, setUserName] = useState('');
   const [userId, setUserId] = useState(-1);
@@ -11,31 +13,31 @@ const App = ({}) => {
   const checkUserAPI = async () => {
     var apiResponse = {};
     const requestOptions = {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
     };
-    if(username==''){
+    if (username === '') {
       return {};
     }
-    await fetch('http://127.0.0.1:5000/checkuser/'+username, requestOptions)
-    .then(async response => {
+    await fetch('http://127.0.0.1:5000/checkuser/' + username, requestOptions)
+      .then(async response => {
         const resp = await response.json();
         if (response.status === 200) {
           apiResponse = resp
         } else {
-            console.log("Error with API")
+          console.log("Error with API")
         }
-    })  
-    .catch(error => {
+      })
+      .catch(error => {
         console.log(error);
-    });
+      });
     console.log(apiResponse);
     return apiResponse;
   }
 
   const checkUser = async () => {
     const apiResponse = await checkUserAPI();
-    
+
     if ("data" in apiResponse) {
       setUserId(apiResponse['data']['id']);
       // getTweetsAPI();
@@ -51,30 +53,27 @@ const App = ({}) => {
   const getTweetsAPI = async () => {
     var apiResponse = {};
     const requestOptions = {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' }
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' }
     };
-    await fetch('http://127.0.0.1:5000/getLatestTweets/'+userId, requestOptions)
-    .then(async response => {
+    await fetch('http://127.0.0.1:5000/getLatestTweets/' + userId, requestOptions)
+      .then(async response => {
         const resp = await response.json();
         if (response.status === 200) {
           apiResponse = resp
         } else {
-            console.log("Error with API")
+          console.log("Error with API")
         }
-    })  
-    .catch(error => {
+      })
+      .catch(error => {
         console.log(error);
-    });
+      });
     console.log(apiResponse);
     setTweets(apiResponse);
-    // var tweetsArray = []
-    // for (let x in apiResponse['data']) {
-    //   txt += numbers[x];
-    // }
+
     const fileName = "tweets";
     const json = JSON.stringify(apiResponse);
-    const blob = new Blob([json],{type:'application/json'});
+    const blob = new Blob([json], { type: 'application/json' });
     const href = await URL.createObjectURL(blob);
     const link = document.createElement('a');
     link.href = href;
@@ -82,41 +81,33 @@ const App = ({}) => {
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
-    // return apiResponse;
-  }
-
-  const downloadFile = async () => {
-    const fileName = "tweets";
-    const json = JSON.stringify(tweets);
-    const blob = new Blob([json],{type:'application/json'});
-    const href = await URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.href = href;
-    link.download = fileName + ".json";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
-
-    return (
-      <div>
-        <button><a href={href} download={fileName + ".json"}>Download Tweets</a></button>
-      </div>
-    )
 
   }
 
   return (
-    <div>
-      <input type="text" placeholder="Enter user name" value={username} onInput={(e) => setUserName(e.target.value)}></input>
-      <button onClick={checkUser}>Check</button>
-      {userId !== -1 ? 
-      <div>
-        <p>User Found</p>
-        <button onClick={getTweetsAPI}>Get latest Tweets</button>
-      </div>
-      : <p></p>}
-    </div>
+    <Container className="input-pad" fluid="md">
+      <Row className="justify-content-md-center">
+        <Col lg={true}>
+        <InputGroup className="mb-3">
+          <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+          <FormControl
+            placeholder="Enter user name"
+            aria-label="Username"
+            aria-describedby="basic-addon1"
+            value={username} onInput={(e) => setUserName(e.target.value)}
+          />
+          <Button variant="primary" onClick={checkUser}>Check</Button>
+        </InputGroup>
+          {userId !== -1 ?
+            <div>
+              <p className="large-font">User Found</p>
+              <Button variant="success" onClick={getTweetsAPI}>Get latest Tweets</Button>{' '}
+            </div>
+            : <p></p>}
+        </Col>
+      </Row>
+    </Container>
   );
-  }
+}
 
-export { App }; 
+export { App };
